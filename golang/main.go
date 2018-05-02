f740e06a30f9168b52d9fd23c28acf39cac7b612
Matthew Fisher <matt.fisher@microsoft.com>
2018-05-02 10:25:14 +0200

    added mongodb sample code

--- golang/main.go
+++ golang/main.go
@@ -2,16 +2,41 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
+var session *mgo.Session
+
+// Hero is a superhero.
+type Hero struct {
+	Name        string
+	Img         string
+	Description string
+	Aliases     []string
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	n, _ := os.Hostname()
-	fmt.Fprintf(w, "Draft is really cool! I am on %s!", n)
+	c := session.DB("webratings").C("heroes")
+	result := Hero{}
+	if err := c.Find(bson.M{"name": "Wonder Woman"}).One(&result); err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	fmt.Fprintf(w, "Aliases for %s: %v\n", result.Name, result.Aliases)
 }
 
 func main() {
+	session, err := mgo.Dial(os.Getenv("MONGO_CONNECTIONSTRING"))
+	if err != nil {
+		log.Fatalf("could not retrieve a connection to mongodb: %v", err)
+	}
+	defer session.Close()
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
