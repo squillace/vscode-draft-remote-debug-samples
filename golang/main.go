f37407e76f7e04930705a89f3a6ec2eacb42f7b6
Ralph Squillace <ralph@squillace.com>
2018-05-31 22:46:58 -0700

    modification

--- golang/main.go
+++ golang/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"crypto/tls"
+	//"crypto/tls"
 	"fmt"
-	"log"
-	"net"
+	//"log"
+	//"net"
 	"net/http"
-	"os"
-	"time"
+	//"os"
+	//"time"
 
 	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
+	//"gopkg.in/mgo.v2/bson"
 )
 
 var session *mgo.Session
@@ -24,49 +24,50 @@ type Hero struct {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-/*
-	c := session.DB("webratings").C("heroes")
-	result := Hero{}
-	if err := c.Find(bson.M{"name": "Wonder Woman"}).One(&result); err != nil {
-		log.Println(err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+	/*
+		c := session.DB("webratings").C("heroes")
+		result := Hero{}
+		if err := c.Find(bson.M{"name": "Wonder Woman"}).One(&result); err != nil {
+			log.Println(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 
-	fmt.Fprintf(w, "%s:  %v\n", result.Name, result.Description)
-*/
+		fmt.Fprintf(w, "%s:  %v\n", result.Name, result.Description)
+	*/
+
 	fmt.Fprintln(w, "Hi Kubecon EU.")
 }
 
 func main() {
-/*
-	mongoHost := os.Getenv("MONGO_HOST")
-	fmt.Printf("mongo host: %s", mongoHost)
-	mongoUsername := os.Getenv("MONGO_USERNAME")
-	mongoPassword := os.Getenv("MONGO_PASSWORD")
-	mongoPort := os.Getenv("MONGO_PORT")
-	dialInfo := &mgo.DialInfo{
-		Addrs: []string{
-			fmt.Sprintf(
-				"%s:%s",
-				mongoHost,
-				mongoPort,
-			),
-		},
-		Timeout:  60 * time.Second,
-		Database: "webratings",
-		Username: mongoUsername,
-		Password: mongoPassword,
-		DialServer: func(addr *mgo.ServerAddr) (net.Conn, error) {
-			return tls.Dial("tcp", addr.String(), &tls.Config{})
-		},
-	}
-	var err error
-	session, err = mgo.DialWithInfo(dialInfo)
-	if err != nil {
-		log.Fatalf("could not retrieve a connection to mongodb: %v", err)
-	}
-	defer session.Close()
-*/
+	/*
+		mongoHost := os.Getenv("MONGO_HOST")
+		fmt.Printf("mongo host: %s", mongoHost)
+		mongoUsername := os.Getenv("MONGO_USERNAME")
+		mongoPassword := os.Getenv("MONGO_PASSWORD")
+		mongoPort := os.Getenv("MONGO_PORT")
+		dialInfo := &mgo.DialInfo{
+			Addrs: []string{
+				fmt.Sprintf(
+					"%s:%s",
+					mongoHost,
+					mongoPort,
+				),
+			},
+			Timeout:  60 * time.Second,
+			Database: "webratings",
+			Username: mongoUsername,
+			Password: mongoPassword,
+			DialServer: func(addr *mgo.ServerAddr) (net.Conn, error) {
+				return tls.Dial("tcp", addr.String(), &tls.Config{})
+			},
+		}
+		var err error
+		session, err = mgo.DialWithInfo(dialInfo)
+		if err != nil {
+			log.Fatalf("could not retrieve a connection to mongodb: %v", err)
+		}
+		defer session.Close()
+	*/
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
