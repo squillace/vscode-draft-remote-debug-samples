ebc370fec8f6402c25c2474a63b7396c445b13f9
Ralph Squillace <ralph@squillace.com>
2018-06-01 00:01:12 -0700

    got it figured out

--- brigade.js
+++ brigade.js
@@ -15,7 +15,7 @@ events.on("push", (brigadeEvent, project) => {
     console.log(`==> gitHub webook with commit ID ${gitSHA}`)
 
     // Let's notice the event
-    var slackJob = new Job("slack-notify", "technosophos/slack-notify:latest", ["/slack-notify"])
+    var slackJob = new Job("slack-notify-github-push", "technosophos/slack-notify:latest", ["/slack-notify"])
     slackJob.storage.enabled = false
     slackJob.env = {
       SLACK_WEBHOOK: project.secrets.slackWebhook,
@@ -40,7 +40,7 @@ events.on("push", (brigadeEvent, project) => {
     ]
 
     // Let's notice the event and test someting after the build.
-    var slackJobTest1 = new Job("slack-notify", "technosophos/slack-notify:latest", ["/slack-notify"])
+    var slackJobTest1 = new Job("slack-notify-image-built", "technosophos/slack-notify:latest", ["/slack-notify"])
     slackJobTest1.storage.enabled = false
     slackJobTest1.env = {
       SLACK_WEBHOOK: project.secrets.slackWebhook,
@@ -50,17 +50,17 @@ events.on("push", (brigadeEvent, project) => {
     }
 
     // Let's notice the event and test someting after the build.
-    var slackJobTest2 = new Job("slack-notify-test2", "technosophos/slack-notify:latest", ["/slack-notify"])
+    var slackJobTest2 = new Job("slack-notify-test", "technosophos/slack-notify:latest", ["/slack-notify"])
     slackJobTest2.storage.enabled = false
     slackJobTest2.env = {
       SLACK_WEBHOOK: project.secrets.slackWebhook,
       SLACK_USERNAME: "brigade-demo",
-      SLACK_MESSAGE: "Running image test 2 ...",
+      SLACK_MESSAGE: "Running container image test ...",
       SLACK_COLOR: "#0000ff"
     }
 
     // Let's notice the event and test someting after the build.
-    var slackJobTest3 = new Job("slack-notify", "technosophos/slack-notify:latest", ["/slack-notify"])
+    var slackJobTest3 = new Job("slack-notify-test-again", "technosophos/slack-notify:latest", ["/slack-notify"])
     slackJobTest3.storage.enabled = false
     slackJobTest3.env = {
       SLACK_WEBHOOK: project.secrets.slackWebhook,
@@ -78,9 +78,9 @@ events.on("push", (brigadeEvent, project) => {
     ]
 
     var pipeline = new Group()
-    //pipeline.add(slackJob)
+    pipeline.add(slackJob)
     pipeline.add(docker)
-    //pipeline.add(slackJobTest1)
+    pipeline.add(slackJobTest1)
     pipeline.add(slackJobTest2)
     pipeline.add(helm)
     
@@ -89,7 +89,7 @@ events.on("push", (brigadeEvent, project) => {
 
 events.on("after", (event, project) => {
     
-    var slack = new Job("slack-notify", "technosophos/slack-notify:latest", ["/slack-notify"])
+    var slack = new Job("slack-notify-after", "technosophos/slack-notify:latest", ["/slack-notify"])
     slack.storage.enabled = false
     slack.env = {
       SLACK_WEBHOOK: project.secrets.slackWebhook,
