c49597a45ca9031a6f926c1ae70e4d3b1d990946
Ralph Squillace <ralph@squillace.com>
2018-06-19 17:54:27 -0700

    updating the demo

--- golang/main.go
+++ golang/main.go
@@ -35,7 +35,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "%s:  %v\n", result.Name, result.Description)
 	*/
 
-	fmt.Fprintln(w, "Hi OSS Summit Japan fans!")
+	fmt.Fprintln(w, "Hi OSS Summit Japan fans, from Tokyo!")
 }
 
 func main() {
